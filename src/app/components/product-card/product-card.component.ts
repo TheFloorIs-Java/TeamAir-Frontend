9413eb3a3bc5bc3c28c4259ac8b41183baf44a53
Jacob Nottoli <jacob.nottoli@gmail.com>
2022-10-19 12:02:30 -0500

    input validation for add to cart

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -8,9 +8,9 @@ import { ProductService } from 'src/app/services/product.service';
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css']
 })
-export class ProductCardComponent implements OnInit{
+export class ProductCardComponent implements OnInit {
 
-  quantity : number = 0;
+  quantity: number = 0;
   cartCount!: number;
   products: {
     product: Product,
@@ -18,11 +18,12 @@ export class ProductCardComponent implements OnInit{
   }[] = [];
   subscription!: Subscription;
   totalPrice: number = 0;
+  quantityerror: boolean = false;
 
   @Input() productInfo!: Product;
 
   constructor(private productService: ProductService) { }
-  
+
   ngOnInit(): void {
     this.subscription = this.productService.getCart().subscribe(
       (cart) => {
@@ -36,38 +37,45 @@ export class ProductCardComponent implements OnInit{
   addToCart(product: Product): void {
 
     if (this.quantity != 0) {
-      let inCart = false;
-      this.products.forEach(
-        (element) => {
-          if(element.product == product){
-            element.quantity = element.quantity + this.quantity;
-            let cart = {
-              cartCount: this.cartCount + this.quantity,
-              products: this.products,
-              totalPrice: this.totalPrice + product.price*this.quantity
+
+      if (this.quantity <= this.productInfo.quantity) {
+        this.quantityerror = false;
+        let inCart = false;
+        this.products.forEach(
+          (element) => {
+            if (element.product == product) {
+              element.quantity = element.quantity + this.quantity;
+              let cart = {
+                cartCount: this.cartCount + this.quantity,
+                products: this.products,
+                totalPrice: this.totalPrice + product.price * this.quantity
+              };
+              this.productService.setCart(cart);
+              inCart = true;
+              return;
             };
-            this.productService.setCart(cart);
-            inCart=true;
-            return;
+          }
+        );
+
+        if (inCart == false) {
+          let newProduct = {
+            product: product,
+            quantity: this.quantity
           };
+          this.products.push(newProduct);
+          let cart = {
+            cartCount: this.cartCount + this.quantity,
+            products: this.products,
+            totalPrice: this.totalPrice + product.price * this.quantity
+          }
+          this.productService.setCart(cart);
         }
-      );
-  
-      if(inCart == false){
-        let newProduct = {
-          product: product,
-          quantity: this.quantity
-        };
-        this.products.push(newProduct);
-        let cart = {
-          cartCount: this.cartCount + this.quantity,
-          products: this.products,
-          totalPrice: this.totalPrice + product.price*this.quantity
-        }
-        this.productService.setCart(cart);
+      } else {
+        this.quantityerror = true;
       }
+
     }
-    
+
   }
 
   ngOnDestroy() {
