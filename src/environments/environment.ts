a4cb8271f8f7504ff56ad5edae7518ece1f70856
Harveen G <harveen096@revature.net>
2022-10-19 13:10:31 -0700

    added front end site in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   baseUrl: "https://ecommerce-app-team-air.azurewebsites.net",
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://localhost:4200',
+    'Access-Control-Allow-Origin': 'https://team-air-frontend.azurewebsites.net',
   },
 };
 
