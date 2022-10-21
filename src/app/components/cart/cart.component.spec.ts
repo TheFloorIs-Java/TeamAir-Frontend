c6ab4f1cfb7b3d6314ea35f1468ac8ea58614808
Ilma Shaharin <shaharinilma96@gmail.com>
2022-10-21 15:18:06 -0400

    minor change in the test case

--- src/app/components/cart/cart.component.spec.ts
+++ src/app/components/cart/cart.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CartComponent } from './cart.component';
-import { Product } from 'src/app/models/product';
 
 describe('CartComponent', () => {
   let component: CartComponent;
@@ -11,7 +10,6 @@ describe('CartComponent', () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       declarations: [CartComponent],
-      // providers: [{ provide: Router, useValue: mockRouter }],
     }).compileComponents();
   });
 
@@ -24,11 +22,13 @@ describe('CartComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
   it('should empty the cart', () => {
     component.cartCounts = 5;
     component.emptyCart();
     expect(component.cartCounts).toBe(0);
   });
+
   it('should remove an item from cart', () => {
     component.totalPrice = 105;
     component.cartCounts = 3;
@@ -40,7 +40,7 @@ describe('CartComponent', () => {
           quantity: 3,
           price: 40,
           description: 'A nice winter coat',
-          image: 'blah',
+          image: 'blank',
           reviews: [],
         },
         quantity: 2,
@@ -52,7 +52,7 @@ describe('CartComponent', () => {
           quantity: 2,
           price: 25,
           description: 'A cool sunglass',
-          image: 'blah',
+          image: 'blank',
           reviews: [],
         },
         quantity: 1,
