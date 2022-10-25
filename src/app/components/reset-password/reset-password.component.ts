630adf7b4a3f0d86a32a3addefd559ed82ae7b28
Ilma Shaharin <shaharinilma96@gmail.com>
2022-10-25 16:32:49 -0400

    doc on reset password component

--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -9,44 +9,50 @@ import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.css']
+  styleUrls: ['./reset-password.component.css'],
 })
 export class ResetPasswordComponent implements OnInit {
-
-  currentPassword : string = "";
-  newPassword : string = "";
-  newPasswordReEnter : string = "";
-  message :string = "";
+  currentPassword: string = '';
+  newPassword: string = '';
+  newPasswordReEnter: string = '';
+  message: string = '';
   authUrl: string = `${environment.baseUrl}/auth`;
 
-  constructor(private authService: AuthService, private router: Router, private http: HttpClient) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  /**
+   * This method allows the users to change their password
+   */
   changePassword() {
-  this.authService.login(this.authService.email,this.currentPassword).subscribe(
-    (data) => {
-      this.message = "";
-      if (this.newPassword == this.newPasswordReEnter) {
-        let user : User = {
-          id: this.authService.id,
-          email : "",
-          password: this.newPassword,
-          firstName : "",
-          lastName: "",
-          userOrAdmin: ""
+    this.authService
+      .login(this.authService.email, this.currentPassword)
+      .subscribe(
+        (data) => {
+          this.message = '';
+          if (this.newPassword == this.newPasswordReEnter) {
+            let user: User = {
+              id: this.authService.id,
+              email: '',
+              password: this.newPassword,
+              firstName: '',
+              lastName: '',
+              userOrAdmin: '',
+            };
+            this.authService.changePassword(user);
+          } else {
+            this.message = "Passwords don't match!";
+          }
+        },
+        (err) => {
+          console.log(err);
+          this.message = 'Incorrect current password!';
         }
-        this.authService.changePassword(user);
-
-      } else {
-        this.message = "Passwords don't match!"
-      }
-    },
-    (err) => 
-    {console.log(err); this.message = "Incorrect current password!"},
-    
-  );
-  
+      );
   }
 }
