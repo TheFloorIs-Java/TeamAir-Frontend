0acd7e9d1e0f6c35d5353de2ab26110e4127ae3b
Jacob Nottoli <jacob.nottoli@gmail.com>
2022-10-26 11:25:21 -0500

    reset password test

--- src/app/components/reset-password/reset-password.component.spec.ts
+++ src/app/components/reset-password/reset-password.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
 import { ResetPasswordComponent } from './reset-password.component';
 
 describe('ResetPasswordComponent', () => {
@@ -8,6 +9,7 @@ describe('ResetPasswordComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], 
       declarations: [ ResetPasswordComponent ]
     })
     .compileComponents();
--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
-
 import { environment } from 'src/environments/environment';
 
 @Component({
